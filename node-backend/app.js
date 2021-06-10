86cb4659b6a578bea16c7347d9b69563d86c37f3
Vinjit Regulagedda <regulagedda.1999@gmail.com>
2021-06-10 01:03:18 -0700

    'FirstCommit'

---
+++ node-backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var path=require('path');
+const request=require('request');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
